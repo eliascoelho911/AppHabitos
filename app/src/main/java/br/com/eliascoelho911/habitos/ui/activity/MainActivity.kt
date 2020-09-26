c839f7cbf10d549e003655c37f0192e3d9089ccd
Elias <elias_coelho911@hotmail.com>
2020-09-25 21:14:51 -0300

    Inserindo calendario horizontal

--- app/src/main/java/br/com/eliascoelho911/habitos/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/eliascoelho911/habitos/ui/activity/MainActivity.kt
@@ -2,10 +2,13 @@ package br.com.eliascoelho911.habitos.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.eliascoelho911.habitos.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(activity_main_toolbar)
     }
 }
--- app/src/main/java/br/com/eliascoelho911/habitos/ui/fragment/TelaHabitosFragment.kt
+++ app/src/main/java/br/com/eliascoelho911/habitos/ui/fragment/TelaHabitosFragment.kt
@@ -1,4 +1,20 @@
 package br.com.eliascoelho911.habitos.ui.fragment
 
-class TelaHabitosFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.eliascoelho911.habitos.R
+
+class TelaHabitosFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_tela_habitos, container, false)
+    }
+
 }
