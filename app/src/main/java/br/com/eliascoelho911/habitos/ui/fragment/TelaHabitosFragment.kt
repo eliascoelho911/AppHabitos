1a9ae9ea8dfb469482ecab3ea5b0210d1eaf7592
Elias <elias_coelho911@hotmail.com>
2020-09-27 22:54:53 -0300

    Exibindo saudação

--- app/src/main/java/br/com/eliascoelho911/habitos/ui/fragment/TelaHabitosFragment.kt
+++ app/src/main/java/br/com/eliascoelho911/habitos/ui/fragment/TelaHabitosFragment.kt
@@ -6,19 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.eliascoelho911.habitos.R
+import br.com.eliascoelho911.habitos.databinding.FragmentTelaHabitosBinding
 import br.com.eliascoelho911.habitos.extensions.formata
 import br.com.eliascoelho911.habitos.ui.viewmodel.MainActivityViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class TelaHabitosFragment : Fragment() {
     private val mainActivityViewModel: MainActivityViewModel by sharedViewModel()
+    private lateinit var binding: FragmentTelaHabitosBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tela_habitos, container, false)
+        binding = FragmentTelaHabitosBinding.inflate(inflater, container, false)
+        binding.saudacao = "Bem vindo, Elias!"
+        return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
